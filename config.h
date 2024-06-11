9dcf9cc9ff48c65751075d23e362c7d5cd4bbb24
Sahil Gautam <sahil@libreoffice.org>
2024-06-11 22:56:09 +0530

    Minimal looking dwm

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int borderpx = 0; /* border pixel of windows */
 static const unsigned int snap = 4;     /* snap pixel */
 static const int swallowfloating =
     1; /* 1 means swallow floating windows by default */
-static const unsigned int gappih = 20; /* horiz inner gap between windows */
-static const unsigned int gappiv = 10; /* vert inner gap between windows */
+static const unsigned int gappih = 0; /* horiz inner gap between windows */
+static const unsigned int gappiv = 0; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    0; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    30; /* vert outer gap between windows and screen edge */
+    0; /* vert outer gap between windows and screen edge */
 static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning =
@@ -22,8 +22,8 @@ static const unsigned int systrayspacing = 0; /* systray spacing */
 static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
-static const int showsystray = 1; /* 0 means no systray */
-static const int showbar = 1;     /* 0 means no bar */
+static const int showsystray = 0; /* 0 means no systray */
+static const int showbar = 0;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
 static const char *fonts[] = {
     "JetBrainsMono Nerd "
