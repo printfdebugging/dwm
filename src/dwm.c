ef435f97a5746969be18a7ab13aed2a2a4137149
Sahil Gautam <sahil.gautam@collabora.com>
2025-09-04 23:00:20 +0530

    dwm bar on the top

--- src/dwm.c
+++ src/dwm.c
@@ -353,8 +353,8 @@ static const unsigned int systrayspacing = 2;  /* systray spacing */
 static const int systraypinningfailfirst =
     1;                              /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray   = 1; /* 0 means no systray */
-static const int topbar        = 0; /* 0 means bottom bar */
-static const char *fonts[]     = {"JetBrainsMono Nerd Font:size=15"};
+static const int topbar        = 1; /* 0 means bottom bar */
+static const char *fonts[]     = {"JetBrainsMono Nerd Font:size=13"};
 static const char col_gray1[]  = "#222222";
 static const char col_gray2[]  = "#444444";
 static const char col_gray3[]  = "#bbbbbb";
