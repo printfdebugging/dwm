12014281096d0bcd7bb929b6513e9cc3d9ae52fb
Sahil Gautam <sahil.gautam@collabora.com>
2025-09-09 06:18:19 +0530

    add OpenGL titled window as a floating one

--- src/dwm.c
+++ src/dwm.c
@@ -402,6 +402,7 @@ static const Rule rules[] = {
 
     {"pavucontrol",    NULL, NULL,           0,      1, 0, 0,  -1},
     {"flameshot",      NULL, NULL,           0,      1, 0, 0,  -1},
+    {NULL,             NULL, "OpenGL",       0,      1, 0, 0,  -1},
     {NULL,             NULL, "Event Tester", 0,      0, 0, 1,  -1}, /* xev */
 };
 
